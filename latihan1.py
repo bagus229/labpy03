ff169c5d919ce9a876754c4418db480b5943c669
Bagus Aditya Hermawan <bagus.aditya.h.06@gmail.com>
2024-10-29 08:55:43 +0700

    menambahkan file

--- latihan1.py
+++ latihan1.py
@@ -1,12 +1,9 @@
 import random
 
-# Meminta input dari pengguna
 n = int(input("Masukkan nilai N: "))
 
-# Inisialisasi variabel untuk menghitung jumlah bilangan acak yang dihasilkan
 count = 0
 
-# Menghasilkan bilangan acak kurang dari 0.5
 while count < n:
     angka = random.random()
     if angka < 0.5:
--- latihan2.py
+++ latihan2.py
@@ -1,17 +1,12 @@
-# Modal awal
 modal_awal = 100000000  # 100 juta
 
-# Persentase laba per bulan selama 8 bulan
-laba_persen = [0, 0, 0.01, 0.01, 0.05, 0.05, 0.05, 0.03]
+laba_persen = [0, 0, 0.01, 0.01, 0.05, 0.05, 0.05, 0.02]
 
-# Inisialisasi total laba
 total_laba = 0
 
-# Loop untuk menghitung laba per bulan
 for bulan in range(8):
     laba_bulan = modal_awal * laba_persen[bulan]
     total_laba += laba_bulan
     print(f"laba bulan ke-{bulan+1} sebesar: {laba_bulan}")
 
-# Menampilkan total laba setelah 8 bulan
 print(f"Total laba adalah: {total_laba}")
